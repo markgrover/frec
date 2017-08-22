8473ca90e85c8f3084c6caf7a898ad0d9c4a80ea
Mark Grover <mark@apache.org>
2017-08-22 14:50:30 -0700

    Adding mysql drive and a main go file

---
+++ src/frec/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	//"fmt"
+	"log"
+)
+
+func main() {
+	db, err := sql.Open("mysql",
+		"user:password@tcp(127.0.0.1:3306)/frec")
+	if err != nil {
+		log.Fatalf("Couldn't connect to db: %s", err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Couldn't connect to db: %s", err)
+	}
+	defer db.Close()
+}
