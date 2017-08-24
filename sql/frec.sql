ffc341fc1104e0f4a49e540bcb1ed9fbf4c58592
Mark Grover <mark@apache.org>
2017-08-23 20:10:25 -0700

    Adding the target allocation

--- sql/frec.sql
+++ sql/frec.sql
@@ -1,8 +1,44 @@
 USE frec;
-CREATE TABLE IF NOT EXISTS accounts (id BIGINT NOT NULL AUTO_INCREMENT, name CHAR(32), PRIMARY KEY(id));
+INSERT INTO account 
+            (name) 
+VALUES      ('Vanguard ROTH IRA Brokerage'); 
 
-CREATE TABLE IF NOT EXISTS categories(id BIGINT NOT NULL AUTO_INCREMENT, name CHAR(32), PRIMARY KEY(id));
+INSERT INTO category 
+            (name) 
+VALUES      ('US Stocks'), 
+            ('VWO'), 
+            ('VB'), 
+            ('VXUS'), 
+            ('Bonds'), 
+            ('Cash'); 
 
--- This should have aid and cid as foreign keys?
-CREATE TABLE IF NOT EXISTS target_alloc(aid, cid, allocation DECIMAL(5,2));
-
+INSERT INTO target_alloc 
+            ( 
+                        aid, 
+                        cid, 
+                        allocation 
+            ) 
+SELECT account.id, 
+       category.id, 
+       alloc 
+FROM   account 
+JOIN 
+       ( 
+              SELECT 'US Stocks' AS name, 
+                     '30.0'      AS alloc 
+              UNION ALL 
+              SELECT 'VWO', 
+                     '10.0' 
+              UNION ALL 
+              SELECT 'VB', 
+                     '20.0' 
+              UNION ALL 
+              SELECT 'VXUS', 
+                     '10.0' 
+              UNION ALL 
+              SELECT 'Bonds', 
+                     '30.0' ) t2 
+JOIN   category 
+ON     ( 
+              t2.name = category.name) 
+where  account.name = 'Vanguard ROTH IRA Brokerage';
--- sql/root.sql
+++ sql/root.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA IF NOT EXISTS frec;
+CREATE SCHEMA IF NOT EXISTS frec CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE USER IF NOT EXISTS 'frec_admin'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL ON frec.* TO 'frec_admin'@'localhost' WITH GRANT OPTION;
 GRANT CREATE USER ON *.* TO 'frec_admin'@'localhost';
